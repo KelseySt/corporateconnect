a4271f7236e21fdf6a6d553c1dcb09a54765ee7a
Kelsey <kelsey.nsterner@gmail.com>
2025-02-08 01:53:27 -0500

    Setup global fonts and colors

---
+++ corporate-connect/src/app/fonts.ts
@@ -0,0 +1,11 @@
+import { Outfit, Roboto_Flex } from 'next/font/google'
+ 
+export const outfit = Outfit({
+  subsets: ['latin'],
+  display: 'swap',
+})
+ 
+export const roboto_flex = Roboto_Flex({
+  subsets: ['latin'],
+  display: 'swap',
+})

c7f74dfe5496e9a1658dee8ba8e97f01fdfcc7d1
Kelsey <kelsey.nsterner@gmail.com>
2025-02-08 10:41:45 -0500

    Created back button function

--- corporate-connect/src/app/learn/[chapter]/[lesson]/[quiz]/page.tsx
+++ corporate-connect/src/app/learn/[chapter]/[lesson]/[quiz]/page.tsx
@@ -88,7 +88,12 @@ export default function Quiz() {
     const quizSelected = params.quiz;
   return (
     <div className="">
-      <h1 className='text-7xl my-10 ml-10'>Quiz {quizSelected} </h1>
+      <div className="flex flex-row gap-5">
+        <Link href="/learn/1" className="my-auto ml-5">
+            <svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" fill="#000000" viewBox="0 0 256 256"><path d="M232,200a8,8,0,0,1-16,0,88.1,88.1,0,0,0-88-88H51.31l34.35,34.34a8,8,0,0,1-11.32,11.32l-48-48a8,8,0,0,1,0-11.32l48-48A8,8,0,0,1,85.66,61.66L51.31,96H128A104.11,104.11,0,0,1,232,200Z"></path></svg>
+        </Link>
+        <h1 className='text-7xl my-10'>Quiz {quizSelected} </h1>
+      </div>
       <div className='block space-y-6 mx-5'>
         
       {quizzes[0].modules[Number(module) - 1].quiz1.map((e) => {
